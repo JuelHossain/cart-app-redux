755c2b6bbef07da50d6a2fff0da979fdad0c800a
juelhossain <juelhossainnn@gmail.com>
2022-08-16 08:47:33 +0600

    total item changed to total quantity

--- src/components/TotalItem.js
+++ src/components/TotalItem.js
@@ -3,7 +3,9 @@ export default function TotalItem({ cart }) {
     <div className="flex justify-center items-center text-center">
       <div className="text-xl font-semibold">
         <p>Total Item</p>
-        <p className="text-5xl">{cart.length}</p>
+        <p className="text-5xl">
+          {cart.reduce((total, current) => total + current.quantity, 0)}
+        </p>
       </div>
     </div>
   );
